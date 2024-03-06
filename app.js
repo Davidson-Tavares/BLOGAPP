8fc67ab4b1bab6eb7781bd24ec004b96940665b2
Davidson-Tavares <davidsonandre1@gmail.com>
2024-03-05 23:49:42 -0300

    cadastro de post

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
 //carregando módulos
-    const express = require('express');
-    const handlebars= require('express-handlebars');
-    const bodyparser= require('body-parser');
-    const app = express();
-    const admin = require("./routes/admin");
-    //const mongoose= require('mongoose');
+const express = require('express');
+const handlebars = require('express-handlebars');
+//const bodyparser = require('body-parser');
+const app = express();
+const admin = require("./routes/admin.js");
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require("connect-flash")
+
 
 //configurações
-    //Body Parser
-        app.use(bodyparser.urlencoded({extended: true}))
-        app.use(bodyparser.json())
-    //Handlebars
-        app.engine('handlebars',handlebars.engine({defaultLayout:'main'}));
-        app.set('view engine','handlebars');
-    //mongoose
+//Body Parser
+//app.use(bodyparser.urlencoded({ extended: true }))
+//app.use(bodyparser.json())
+//essa consfiguração substitui o body parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//sessão
+app.set('trust proxy', 1)
+app.use(session({
+    secret: "curso de node",
+    proxy: true, //adicionei esta
+    resave: true,// estava resove
+    saveUninitialized: true
+}))
+app.use(flash())
+
+//middleware
+//midleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+
+//Handlebars
+app.set('views', path.join(__dirname, 'views'));//adicionei esta
+app.engine('.handlebars', handlebars.engine({
+    defaultLayouts: 'main',
+    extname: '.handlebars'
+}))
+app.set('view engine', '.handlebars')
+
+
+//mongoose
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogapp").then(() => {
+    console.log("Conectado ao Mongo.")
+}).catch((err) => {
+    console.log("Erro ao se conectar :" + err)
+})
+
+//Public
+app.use(express.static(path.join(__dirname + "/public")));
 
 //Rotas
 app.use('/admin', admin);
 
 
 //Outros
-const Port = 8081;
+const Port = 27017;
 app.listen(Port, () => {
-    console.log("Servidor Rodando ! ")
+    console.log("Servidor Rodando na url: http://localhost:27017")
 })
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
+require("../models/Categoria");
+const categoria = mongoose.model("categorias")
 
 router.get('/', (req, res) => {
-res.send("Página principal do painel ADM")
-
+    res.render('admin/index.handlebars')
 })
+//===========================
 
-router.get("/posts" , (req, res)=> {
-res.send("Pagina de posts")
+router.get("/posts", (req, res) => {
+    res.send("Pagina de posts")
 })
+//====================================
 
-router.get('/.categorias', (req, res)=> {
-res.send("Página de categorias")
+router.get('/categorias', (req, res) => {
+    res.render('admin/categorias.handlebars')
+})
+//===============================
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategoria.handlebars')
+})
+//=============================
+
+router.post("/categorias/nova", (req, res) => {
+    var erros = []
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: "Nome inválido !" })
+    }
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({ texto: "Slug inválido" })
+    }
+    if (req.body.nome. length < 2) {
+        erros.push({ texto: "Nome da categoria é muito pequeno !" })
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addcategoria.handlebars', {erros: erros})
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        new categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso !')
+            res.render('admin/categorias.handlebars')
+        }).catch((err) => { 
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria, tente novamente!')
+            res.redirect('/admin')})
+
+    }
 })
 
 module.exports = router
