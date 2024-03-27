58e4e6928c6caaaa056ef726382c5de41ca7891b
Davidson-Tavares <davidsonandre1@gmail.com>
2024-03-27 11:13:18 -0300

    Usuarios e registro
    iniciei o sistema te autenticação e cadastro  de usuários.

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const session = require('express-session');
 const flash = require("connect-flash")
 require("./models/Postagem");
 const postagem = mongoose.model("postagens")
+require("./models/Categoria.js")
+const categoria = mongoose.model("categorias")
+const usuarios = require("./routes/usuario.js")
 
 
 //configurações
@@ -29,7 +32,7 @@ app.use(session({
     saveUninitialized: true
 }))
 
-   
+
 //middleware
 app.use(flash())
 app.use((req, res, next) => {
@@ -61,16 +64,56 @@ mongoose.connect("mongodb://127.0.0.1:27017/blogapp").then(() => {
 app.use(express.static(path.join(__dirname + "/public")));
 
 //Rotas
-/*app.get('/', (req, res) => {
-    postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens) => {
-        res.render("admin/index.handlebars", {postagens: postagens})
-    }).catch((err) =>{
-        req.flash('error_msg',"Houve um erro interno ! " )
-        res.redirect('/404')
-    } )
- 
+//------------ rota para pagina de postagem com cada pagina sendo criada atraves do slug.
 
-})*/
+
+app.get("/postagem/:slug", (req, res) => {
+    postagem.findOne({ slug: req.params.slug }).lean().then((postagem) => {
+        if (postagem) {
+            res.render("postagem/index.handlebars", { postagem: postagem })
+        } else {
+            req.flash("error_msg", "Esta postagem não existe !")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/")
+    })
+})
+
+//=========== rota da pagina categorias =============
+
+app.get("/categorias", (req, res) => {
+    categoria.find().lean().then((categorias) => {
+        res.render("categorias/index.handlebars", { categorias: categorias })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao listar categorias")
+        res.redirect("/")
+    })
+})
+
+// ===== rota para os links de cada categoria da pagina categoria
+
+app.get("/categorias/:slug", (req, res) => {
+    categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+        if (categoria) {
+            postagem.find({ categoria: categoria._id }).lean().then((postagens) => {
+                res.render("categorias/postagens", { postagens: postagens, categoria: categoria })
+            }).catch((err) => {
+                console.log("Eroo:: " + err)
+                req.flash("error_msg", "Houve um erro ao listar os posts !")
+                res.redirect("/")
+            })
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe !")
+        res.redirect("/")
+    })
+})
+
+
+
+//======================================================lista postagem
 app.get("/", (req, res) => {
     postagem.find().lean().populate("categoria").sort({ data: "desc" }).then((postagens) => {
         res.render("admin/index", { postagens: postagens })
@@ -88,12 +131,15 @@ app.get('/404', (req, res) => {
 
 
 //--------------------------------------------------------------------
-app.get('/posts', (req,res) =>{
+app.get('/posts', (req, res) => {
     res.send("Lista de posts")
 })
 
 //---------------------------------------------------------------
+
+
 app.use('/admin', admin);
+app.use('/usuarios', usuarios)
 
 
 
---
+++ routes/usuario.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Usuario')
+const Usuario = mongoose.model("usuarios")
+
+router.get("/registro", (req, res)=> {
+    res.render('usuarios/registro.handlebars')
+})
+
+
+
+
+
+
+
+
+module.exports= router
